13f5ce9763f2fd63f29b2400c105692c101283c2
Rockett <79261584+Rockeett@users.noreply.github.com>
2021-06-26 12:33:49 +0100

    Possible fix - not sure - DON'T CARE TBF

--- qb-kickout/client.lua
+++ qb-kickout/client.lua
@@ -1,36 +1,35 @@
 QBCore = nil
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(10)
-        if QBCore == nil then
-            TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-            Citizen.Wait(200)
-        end
+Citizen.CreateThread(function() 
+    while QBCore == nil do
+        TriggerEvent("QBCore:GetObject", function(obj) QBCore = obj end)    
+        Citizen.Wait(200)
     end
 end)
 
--- kick car stealing cunts out of cop cars
+-- code
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(5000)
+		Citizen.Wait(100)
 
 		local ped = PlayerPedId(-1)
 		local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
 		local vehicleClass = GetVehicleClass(vehicle)
-		local driver = GetPedInVehicleSeat(vehicle, -1)
-		PlayerJob = QBCore.Functions.GetPlayerData().job
+		local playerSeat = GetLastPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), false), 1)
+		local PlayerJob = QBCore.Functions.GetPlayerData().job
 
     	if PlayerJob.name ~= "police" or PlayerJob.name ~= "ambulance" or PlayerJob.name ~= "mechanic" then  
 
 			if GetVehicleClass(vehicle) == 18 then
-				if IsPedInAnyVehicle(PlayerPedId(-1), true) then
+				if GetLastPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), true), 1) == true then
 
-					TaskLeaveVehicle(ped, vehicle, 64)
-					SetVehicleDoorsLocked(vehicle, 2)
-					QBCore.Functions.Notify('You are not allowed to drive this vehicle', 'error')
-					Citizen.Wait(500)
+					if IsPedInAnyVehicle(PlayerPedId(-1), true) then
+						TaskLeaveVehicle(ped, vehicle, 64)
+						SetVehicleDoorsLocked(vehicle, 2)
+						QBCore.Functions.Notify('Are you a cop? dickhead..', 'error')
+						Citizen.Wait(1500)
+					end
 				end
 			end
 		end
